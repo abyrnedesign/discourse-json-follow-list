ba5c302755ec61b489f405722d0e08ac42727748
Andrew <byrneandrew46@gmail.com>
2016-05-02 13:49:21 +1000

    Update plugin.rb
    Post - > post

--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,7 @@ load File.expand_path('../lib/json_follow_list/engine.rb', __FILE__)
 
 after_initialize do
   require_dependency 'topic'
-  require_dependency 'Post'
+  require_dependency 'post'
 
   Discourse::Application.routes.append do
     mount ::JsonFollowList::Engine, at: "/followlist"
