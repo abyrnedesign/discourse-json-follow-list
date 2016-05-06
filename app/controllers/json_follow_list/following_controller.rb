65194c4157005d3cd9b503eec09fd02490a0d467
Andrew Byrne <abyrne@squiz.net>
2016-05-06 17:05:42 +1000

    building version

--- app/controllers/json_follow_list/following_controller.rb
+++ app/controllers/json_follow_list/following_controller.rb
@@ -3,23 +3,21 @@ module JsonFollowList
 		requires_plugin 'json-follow-list'
 
 		skip_before_filter :check_xhr #allow API requests
-    		before_filter :ensure_logged_in #or just check the users logged in
+    before_filter :ensure_logged_in #or just check the users logged in
 
 		def topics
 			# Magically make arrays from string Wooooo!
 			uid = params["uid"].split(",")
-			tid =  params["tid"].split(",")
+			tid = params["tid"].split(",")
 
 			#request post from usrs AND specific topics ahhhhhhh!
-			posts = Post.where('user_id IN (?) OR topic_id IN (?)', uid, tid )
-					.order(created_at: :desc)
-					.limit(params["total"])
-					.offset(params["start"])
+			posts = Post.where('user_id IN (?) OR topic_id IN (?) OR raw LIKE (?)', uid, tid, '%@everyone%' )
+			 									.order(created_at: :desc)
+			                 	.limit(params["total"])
+												.offset(params["start"])
 
-			#Render a json object OMG!
-			obj = {follow_posts: posts }
-			render json: obj
-
+			# that cleaned that up
+			render_json_dump(serialize_data(posts, PostSerializer))
 			#K shows over
 		end
 	end
