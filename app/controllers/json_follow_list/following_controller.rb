38d0e6eb73085781eb0a98c2738d2751d75f10ee
Andrew Byrne <abyrne@squiz.net>
2016-11-23 11:51:22 +1100

    updating for deleted post/topic error

--- app/controllers/json_follow_list/following_controller.rb
+++ app/controllers/json_follow_list/following_controller.rb
@@ -3,49 +3,66 @@ module JsonFollowList
 		requires_plugin 'json-follow-list'
 
 		skip_before_filter :check_xhr #allow API requests
-    before_filter :ensure_logged_in #or just check the users logged in
+		before_filter :block_if_readonly_mode #or just check the users logged in
 
 		def topics
 			# Magically make arrays from string Wooooo!
 
-
 			if params["uid"]
-				uid = params["uid"].split(",")
+				uid = params["uid"].split(",").map(&:to_i)
 			else
 				uid = ''
 			end
 
 			if params["tid"]
-				tid = params["tid"].split(",")
+				tid = params["tid"].split(",").map(&:to_i)
 			else
 				tid = ''
 			end
 
-			if params["usn"]
-				username = "%@" + params["usn"] + "%"
+			if params["usn"] && !params["usn"].empty?
+				username = "%" + params["usn"] + "%"
 			else
 				username = ''
 			end
 
+			## look up the posts
+			excludetopic = Post.unscoped {
+				Post.select('DISTINCT topic_id').where('deleted_at IS NOT NULL').map(&:topic_id)
+			}
+
+			## look up the topics
+			includeUser = Topic.unscoped {
+				Topic.select('DISTINCT id').where('user_id IN (?) AND deleted_at IS NULL', uid).map(&:id)
+			}
+
+			#subtract the bad and add the good
+			tquery = tid - excludetopic
+			tquery =  includeUser + tquery
+
+			#test for a group
 			if params["grp"] && !params["grp"].empty?
 				group = "%@" + params["grp"] + "%"
-				posts = Post.where('user_id IN (?) OR topic_id IN (?) OR raw LIKE (?) OR raw LIKE (?)', uid, tid, username, group)
+
+				posts = Post.unscoped {
+					Post.where(' topic_id IN (?) OR raw LIKE (?) OR raw LIKE (?) AND deleted_at IS NULL AND NOT hidden', tquery, username, group)
 													.order(created_at: :desc)
 													.limit(params["total"])
 													.offset(params["start"])
-
+				}
 			else
-				posts = Post.where('user_id IN (?) OR topic_id IN (?) OR raw LIKE (?)', uid, tid, username)
-													.order(created_at: :desc)
-													.limit(params["total"])
-													.offset(params["start"])
+			 posts = Post.unscoped {
+				 Post.where('topic_id IN (?) OR raw LIKE (?) AND deleted_at IS NULL AND NOT hidden', tquery, username)
+			 									.order(created_at: :desc)
+			 									.limit(params["total"])
+												.offset(params["start"])
+			}
 			end
-
 
 			# that cleaned that up
 			render_json_dump(serialize_data(posts, PostSerializer,
-																						 add_raw: true,
-																						 add_title: true
+																						 add_raw: true
+																						 #add_title: true
 																						 ))
 			#K shows over
 		end
